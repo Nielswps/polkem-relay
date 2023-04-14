0c49c9fdfdfae18173b8c76686fb2dd97cf518f5
Niels Vistisen <nielswps@gmail.com>
2023-04-14 16:28:20 +0200

    Node initialization moved to script, fixed, and extended with additional keys

--- build_init.sh
+++ build_init.sh
@@ -3,7 +3,7 @@
 cargo build --release && \
 cp target/release/polkadot . && \
 
-podman build -t docker.io/nielswps/relay-chain-node-init:latest -f InitContainerDockerfile && \
+podman build -t docker.io/nielswps/relay-chain-node-init:latest -f DockerfileInitContainer && \
 podman push nielswps/relay-chain-node-init && \
 
 rm polkadot
