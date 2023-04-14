e31b67184cb3a1dea90a41014488bf81976397c1
Niels Vistisen <nielswps@gmail.com>
2023-04-14 17:38:22 +0200

    Build.sh updated to not recompile chain specification

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ podman build -t docker.io/nielswps/relay-chain-node:latest -f Dockerfile && \
 podman push nielswps/relay-chain-node && \
 
 #./polkadot build-spec --disable-default-bootnode --chain=rococo-local > relay-chain-spec.json && \
-./polkadot build-spec --disable-default-bootnode --chain=relay-chain-spec.json --raw > raw-relay-chain-spec.json && \
-cp raw-relay-chain-spec.json ../polkem/chain-specs/relay-chain-spec.json && \
+#./polkadot build-spec --disable-default-bootnode --chain=relay-chain-spec.json --raw > raw-relay-chain-spec.json && \
+#cp raw-relay-chain-spec.json ../polkem/chain-specs/relay-chain-spec.json && \
 rm polkadot
